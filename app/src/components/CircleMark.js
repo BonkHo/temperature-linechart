4f285451a174d8b3983f283f9849e9433ca464e7
John Bae <john21225@gmail.com>
2022-01-05 10:41:58 -0800

    Finalized line chart

--- app/src/components/CircleMark.js
+++ app/src/components/CircleMark.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { line } from "d3";
+import { curveNatural, line } from "d3";
 
 const CircleMark = ({
 	data,
@@ -17,7 +17,8 @@ const CircleMark = ({
 				fill="none"
 				d={line()
 					.x((d) => xScale(xValue(d)))
-					.y((d) => yScale(yValue(d)))(data)}
+					.y((d) => yScale(yValue(d)))
+					.curve(curveNatural)(data)}
 			/>
 			{data.map((d) => (
 				<circle cx={xScale(xValue(d))} cy={yScale(yValue(d))} r={markRadius}>
