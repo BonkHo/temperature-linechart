0cfca747da12781f980dd2087a434d6dcd139638
John Bae <john21225@gmail.com>
2022-01-04 17:29:04 -0800

    Line not showing properly

--- app/src/components/CircleMark.js
+++ app/src/components/CircleMark.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { line } from "d3";
 
 const CircleMark = ({
 	data,
@@ -9,20 +10,24 @@ const CircleMark = ({
 	toolTipFormat,
 	markRadius = 8,
 }) => {
-	return;
-	<>
-		<path d={""} />
-		{data.map((d) => (
-			<circle
-				className="mark"
-				cx={xScale(xValue(d))}
-				cy={yScale(yValue(d))}
-				r={markRadius}
-			>
-				<title>{toolTipFormat(xValue(d))}</title>
-			</circle>
-		))}
-	</>;
+	return (
+		<>
+			<path
+				stroke="black"
+				d={line().x((d) => xScale(xValue(d)).y((d) => yScale(yValue(d)))(data))}
+			/>
+			{data.map((d) => (
+				<circle
+					className="mark"
+					cx={xScale(xValue(d))}
+					cy={yScale(yValue(d))}
+					r={markRadius}
+				>
+					<title>{toolTipFormat(xValue(d))}</title>
+				</circle>
+			))}
+		</>
+	);
 };
 
 export default CircleMark;
